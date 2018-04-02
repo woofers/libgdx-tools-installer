0778787c437e75cb386103f58226a36cda7d0e71
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-04-01 17:41:04 -0700

    Fix Link for texture-packer-gui

--- install.sh
+++ install.sh
@@ -25,11 +25,10 @@ cd ..
 wget https://github.com/crashinvaders/gdx-texture-packer-gui/releases/download/4.6.0/gdx-texturepacker-4.6.0.zip
 unzip gdx-texturepacker-4.6.0.zip -d texture-packer-gui
 cd texture-packer-gui
-chmod a+x ./launcher.sh
 
 # Alias Commands
 ln -s "/opt/libgdx-tools/texture-packer" "/usr/bin/texture-packer"
-ln -s "/opt/libgdx-tools/libgdx/texture-packer-gui/launcher.sh" "/usr/bin/texture-packer-gui"
+ln -s "/opt/libgdx-tools/texture-packer-gui" "/usr/bin/texture-packer-gui"
 ln -s "/opt/libgdx-tools/tiled-packer" "/usr/bin/tiled-packer"
 ln -s "/opt/libgdx-tools/fbx-conv-gui" "/usr/bin/fbx-conv-gui"
 ln -s "/opt/libgdx-tools/libgdx/fbx-conv/fbx-conv-lin64" "/usr/bin/fbx-conv"
