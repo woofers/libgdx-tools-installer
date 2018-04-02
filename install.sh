7acc286493253211c4cbc55608932c6307e98a81
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-04-01 17:30:51 -0700

    Add fbx-conv-gui

--- install.sh
+++ install.sh
@@ -9,14 +9,19 @@ wget https://libgdx.badlogicgames.com/nightlies/libgdx-nightly-latest.zip
 unzip libgdx-nightly-latest.zip -d libgdx
 rm libgdx-nightly-latest.zip
 
-# Alias Commands
-ln -s "/opt/libgdx-tools/texture-packer" "/usr/bin/texture-packer"
-ln -s "/opt/libgdx-tools/tiled-packer" "/usr/bin/tiled-packer"
-
 # fbx-conv
 cd libgdx
 wget http://libgdx.badlogicgames.com/fbx-conv/fbx-conv.zip
 unzip fbx-conv.zip -d fbx-conv
-ln -s "/opt/libgdx-tools/libgdx/fbx-conv/fbx-conv-lin64" "/usr/bin/fbx-conv"
 ln -s "/opt/libgdx-tools/libgdx/fbx-conv/libfbxsdk.so" "/usr/lib64/libfbxsdk.so"
 rm fbx-conv.zip
+
+# fbx-conv GUI
+cd fbx-conv
+wget https://github.com/ASneakyFox/libgdx-fbxconv-gui/releases/download/1.5/fbxconv-gui.jar
+
+# Alias Commands
+ln -s "/opt/libgdx-tools/texture-packer" "/usr/bin/texture-packer"
+ln -s "/opt/libgdx-tools/texture-packer" "/usr/bin/texture-packer"
+ln -s "/opt/libgdx-tools/fbx-conv-gui" "/usr/bin/fbx-conv-gui"
+ln -s "/opt/libgdx-tools/libgdx/fbx-conv/fbx-conv-lin64" "/usr/bin/fbx-conv"
