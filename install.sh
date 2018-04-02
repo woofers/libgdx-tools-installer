64f68678d6407b462816303feb94ebc803c6694a
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-04-01 17:38:22 -0700

    Add texture-packer-gui

--- install.sh
+++ install.sh
@@ -19,9 +19,17 @@ rm fbx-conv.zip
 # fbx-conv GUI
 cd fbx-conv
 wget https://github.com/ASneakyFox/libgdx-fbxconv-gui/releases/download/1.5/fbxconv-gui.jar
+cd ..
+
+# texture-packer GUI
+wget https://github.com/crashinvaders/gdx-texture-packer-gui/releases/download/4.6.0/gdx-texturepacker-4.6.0.zip
+unzip gdx-texturepacker-4.6.0.zip -d texture-packer-gui
+cd texture-packer-gui
+chmod a+x ./launcher.sh
 
 # Alias Commands
 ln -s "/opt/libgdx-tools/texture-packer" "/usr/bin/texture-packer"
-ln -s "/opt/libgdx-tools/texture-packer" "/usr/bin/texture-packer"
+ln -s "/opt/libgdx-tools/libgdx/texture-packer-gui/launcher.sh" "/usr/bin/texture-packer-gui"
+ln -s "/opt/libgdx-tools/tiled-packer" "/usr/bin/tiled-packer"
 ln -s "/opt/libgdx-tools/fbx-conv-gui" "/usr/bin/fbx-conv-gui"
 ln -s "/opt/libgdx-tools/libgdx/fbx-conv/fbx-conv-lin64" "/usr/bin/fbx-conv"
