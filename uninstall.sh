34ff44c13b4797dd4cb5b1c55baf06bcbc3d6ed2
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-04-11 14:48:03 -0700

    Uninstall More Components

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,9 @@ cd $GDXTOOLS/libgdx-tools
 
 rm -rf libgdx
 
-rm "/usr/bin/fbx-conv"
 rm "usr/lib64/libfbxsdk.so"
+rm "/usr/bin/texture-packer"
+rm "/usr/bin/texture-packer-gui"
+rm "/usr/bin/tiled-packer"
+rm "/usr/bin/fbx-conv-gui"
+rm "/usr/bin/fbx-conv"
