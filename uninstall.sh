71eff9e8b9cee56cdf5e53add7f9cf42c1b5e148
Jaxson Van Doorn <woofers1tt@gmail.com>
2018-06-26 22:41:49 -0700

    Fix Uninstall Path

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,7 @@ cd $GDXTOOLS/libgdx-tools
 
 rm -rf libgdx
 
-rm "usr/lib64/libfbxsdk.so"
+rm "/usr/lib64/libfbxsdk.so"
 rm "/usr/bin/texture-packer"
 rm "/usr/bin/texture-packer-gui"
 rm "/usr/bin/tiled-packer"
